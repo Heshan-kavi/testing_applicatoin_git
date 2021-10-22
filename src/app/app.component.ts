76a010ad5f897924d106c43666bd1ea0e42cf032
Heshan-kavi <kavindaheshan96@gmail.com>
2021-10-22 16:07:55 +0530

    the secont commit is here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'this is testing application by heshan kavinda';
+  title = 'Hello HEshan';
 }
